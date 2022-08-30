a0109958ca9efb5d1a30b3f2926b3ce20e86a651
MoulikPal <moulik.pal@gloresoft.de>
2022-08-30 22:54:02 +0200

    Spring Boot Skeleton Project

---
+++ WorkOrderApi/src/main/java/de/gloresoft/workorderapi/WorkOrderApiApplication.java
@@ -0,0 +1,13 @@
+package de.gloresoft.workorderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkOrderApiApplication.class, args);
+	}
+
+}
---
+++ WorkOrderApi/src/test/java/de/gloresoft/workorderapi/WorkOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.gloresoft.workorderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
