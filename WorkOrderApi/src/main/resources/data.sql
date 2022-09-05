6f8e2381fad641abecf21d51cc744e3af7db4735
MoulikPal <moulik.pal@gloresoft.de>
2022-09-05 16:09:58 +0200

    Adding mysql connection for dev environment

--- WorkOrderApi/src/main/resources/data.sql
+++ WorkOrderApi/src/main/resources/data.sql
@@ -2,3 +2,5 @@ insert into work_order (id, employee_name, project_number, email_id, date_from, 
 values (1, 'Moulik Pal', '1001', 'moulik.pal@gloresoft.de', '2022-01-01', '2022-04-01', 80);
 insert into work_order (id, employee_name, project_number, email_id, date_from, date_to, working_days)
 values (2, 'Utkarsh Mishra', '1002', 'utkarsh.mishra@gloresoft.de', '2022-02-01', '2022-05-01', 80);
+insert into work_order (id, employee_name, project_number, email_id, date_from, date_to, working_days)
+values (3, 'Gajanan Gaikwad', '1003', 'gajanan.gaikwad@gloresoft.de', '2022-03-01', '2022-09-01', 110);
